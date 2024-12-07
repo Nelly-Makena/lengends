4c6c20a5921bce54c118c2fe03ec68ad5287e89c
NellyMakena <nellysolomonmakena3@gmail.com>
2024-12-07 09:22:26 +0300

    corrections

--- lengends/events/views.py
+++ lengends/events/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render, redirect
 from .forms import EventRegistrationForm
 
-# Handle form submission
 def register_event_view(request):
     if request.method == "POST":
+        # form with POST data
         form = EventRegistrationForm(request.POST)
         if form.is_valid():
-            # Save the form data
+            # Save the form data if valid
             form.save()
-            return redirect('thank_you')  # redirect to thank u page
+            return redirect('thank_you')  # Redirect to thank-you page
     else:
+        #  empty form for GET request
         form = EventRegistrationForm()
 
     return render(request, 'register_event.html', {'form': form})
 
-# Render the thank-you page
 def thank_you_view(request):
     return render(request, 'thank_you.html')
--- lengends/lengends/urls.py
+++ lengends/lengends/urls.py
@@ -4,6 +4,6 @@ from events.views import thank_you_view, register_event_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register-event/', register_event_view, name='register_event'),
+    path('', register_event_view, name='register_event'),
     path('thank-you/', thank_you_view, name='thank_you'),  # Thank You page URL
 ]
